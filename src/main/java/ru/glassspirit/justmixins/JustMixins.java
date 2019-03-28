cfe8dc38c5e63c93eac703b63d8e36791653fc4b
GlassSpirit <yurikochkun@gmail.com>
2019-03-28 18:03:19 +0300

    init

---
+++ src/main/java/ru/glassspirit/justmixins/JustMixins.java
@@ -0,0 +1,14 @@
+package ru.glassspirit.justmixins;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(
+        modid = JustMixins.MOD_ID,
+        name = JustMixins.MOD_NAME,
+        version = JustMixins.VERSION
+)
+public class JustMixins {
+    public static final String MOD_ID = "justmixins";
+    public static final String MOD_NAME = "_JustMixins";
+    public static final String VERSION = "0.7.11";
+}
